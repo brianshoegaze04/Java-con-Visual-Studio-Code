b64383ed230b24ca2767a16bec9e5a1d161ef8d4
Brian Gabriel Hurtado Moreno <140124803+brianshoegaze04@users.noreply.github.com>
2023-09-11 18:34:34 -0500

    Hice algunos cambios en el archivo HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ class HolaMundo {
 
     public static void main(String[] args){
 
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo, desde CodigoFacilito!");
 
     }
 
